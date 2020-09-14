b8ff38c71ef8b62fc51d3fa39f2634b9da1636f8
Cody-Hayes97 <hayesmichael2@gmail.com>
2020-09-14 15:05:53 -0700

    mikroorm setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello world')
+import { MikroORM } from "@mikro-orm/core"
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import microConfig from './mikro-orm.config'
+
+const main = async () => {
+    const orm = await MikroORM.init(microConfig);
+
+    const post = orm.em.create(Post, {title: 'My First Post'})
+    await orm.em.persistAndFlush(post)
+    await orm.em.nativeInsert(Post, {title:'My Second Post'})
+}
+
+
+main().catch(err => {
+    console.log(err)
+});
